27434d68bc504406717b6793acea8cbb3cb02ed4
Manuel <manuel.gutierrezrojas8@gmail.com>
2020-03-02 11:18:20 -0600

    Enabling cordova to run on IOS and Android

--- src/pages/forecast/forecast.ts
+++ src/pages/forecast/forecast.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather-provider';
-import { debugOutputAstAsTypeScript } from '@angular/compiler';
-
 
 @IonicPage()
 @Component({
